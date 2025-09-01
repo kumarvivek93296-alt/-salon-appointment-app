9558818ada88d8b72249179f05898279e4d12a52
kumarvivek93296-alt <kumarvivek93296@gmail.com>
2025-09-01 20:43:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,17 @@ document.getElementById("bookingForm").addEventListener("submit", function(e) {
     `Thanks ${name}! Your ${service} appointment is booked for ${datetime}.`;
 
   this.reset();
+});document.getElementById("bookingForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const service = document.getElementById("service").value;
+  const datetime = document.getElementById("datetime").value;
+
+  document.getElementById("confirmation").innerHTML = 
+    `Thanks ${name}! Your ${service} appointment is booked for ${datetime}.`;
+
+  this.reset();
 });
+
+
