c3b84f04a183d5d16ecf9647856bc55ffbdc058c
kumarvivek93296-alt <kumarvivek93296@gmail.com>
2025-09-01 20:44:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,4 +12,20 @@ self.addEventListener('fetch', function(e) {
       return response || fetch(e.request);
     })
   );
+});self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('salon-app').then(function(cache) {
+      return cache.addAll(['index.html', 'style.css', 'script.js', 'manifest.json']);
+    })
+  );
 });
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+
